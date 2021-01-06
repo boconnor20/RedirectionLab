38304b242e64d9e2ac297c4130865954717ff39f
Brandon Oconnor <boconnor20@stuy.edu>
2021-01-05 23:25:39 -0500

    completed pigLatin()

--- PigLatin.java
+++ PigLatin.java
@@ -17,6 +17,23 @@ public class PigLatin{
     return t.substring(1,t.length()) + t.substring(0,1) + "ay";
   }
   public static String pigLatin(String s){
-    
-  }
+      String t = s.toLowerCase();
+      String[] vowels = new String[]{"a","e","i","o","u"};
+      String[] diagraphs = new String[]{"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+      String stuff = "";
+      if(s.length()>1){
+        stuff = t.substring(0,2);
+      }
+      else{
+        stuff = t.substring(0,1);
+      }
+  
+      for(int i = 0; i < diagraphs.length; i++){
+        if(stuff.equals(diagraphs[i])){
+          return t.substring(2,t.length()) + stuff + "ay";
+        }
+      }
+      return pigLatinSimple(t);
+    }
+
 }
