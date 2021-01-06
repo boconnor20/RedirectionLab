225ad6cafc2f26f17aa57e00f949d7a435f30ca7
Brandon Oconnor <boconnor20@stuy.edu>
2021-01-05 23:58:39 -0500

    added pigLatinBest,working on main

--- PigLatin.java
+++ PigLatin.java
@@ -27,7 +27,7 @@ public class PigLatin{
       else{
         stuff = t.substring(0,1);
       }
-  
+
       for(int i = 0; i < diagraphs.length; i++){
         if(stuff.equals(diagraphs[i])){
           return t.substring(2,t.length()) + stuff + "ay";
@@ -36,4 +36,42 @@ public class PigLatin{
       return pigLatinSimple(t);
     }
 
+    public static String pigLatinBest(String s){
+        String t = s.toLowerCase();
+        String[] alphabets = new String[]{"a","b","c","d","e","f","g","h","i",
+      "j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+        String[] numbers = new String[]{"0","1","2","3","4","6","7","8","9"};
+        boolean punc = false;
+        boolean LLetter = false;
+        boolean LNumber = false;
+        boolean FLetter= true;
+        int count = 0;
+        for(int i = 0; i < alphabets.length;i++){
+          if((t.substring(s.length()-1).equals(alphabets[i]))){
+            FLetter=false;
+          }
+        }
+        if (FLetter){
+          return t;
+        }
+        for(int i = 0; i < alphabets.length;i++){
+          if((t.substring(s.length()-1).equals(alphabets[i]))){
+            LLetter = true;
+          }
+        }
+
+        for(int i = 0; i < numbers.length; i ++){
+          if(((t.substring(s.length()-1).equals(numbers[i])))){
+            LNumber = true;
+          }
+        }
+        //if not is letter and not is number then is punc
+        if(LLetter==false && LNumber==false){
+          punc = true;
+        }
+        if(punc){
+          return pigLatin(t.substring(0,t.length()-1)) + t.substring(t.length()-1);
+        }
+        return pigLatin(t);
+      }
 }
